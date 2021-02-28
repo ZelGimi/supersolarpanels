ecbb08d581d66e73148cd543a2fa17a1d5a926e1
Denfop <42618899+ZelGimi@users.noreply.github.com>
2021-02-28 21:39:41 +0200

    add: Toriy ore

--- src/main/java/com/denfop/ssp/common/BlocksRegister.java
+++ src/main/java/com/denfop/ssp/common/BlocksRegister.java
@@ -15,28 +15,37 @@ public class BlocksRegister {
 
 	public static final Block iridium_Block = new SSPBlock(Material.ROCK, "iridium_block", 8.0F, 15.0F, "pickaxe", 2, SoundType.STONE);
 	public static final Block platiumOre = new SSPOre(Material.ROCK, "platium_ore", 5.0F, 8.0F, "pickaxe", 3, SoundType.STONE);
-
+	public static final Block toriyore = new SSPToriyOre(Material.ROCK, "toriyore", 5.0F, 8.0F, "pickaxe", 3, SoundType.STONE);
 	public static final Block platium_Block = new SSPBlock(Material.ROCK, "platium_block", 8.0F, 15.0F, "pickaxe", 2, SoundType.STONE);
-
+	public static final  Item toriy = new SSPItem("toriy");
 	public static void register() {
 		setRegister(iridiumOre);
 		setRegister(iridium_Block);
 		setRegister(platiumOre);
+		setRegister(toriyore);
 		setRegister(platium_Block);
+		setRegisterItem(toriy);
 	}
 
 	private static void setRegister(Block block) {
 		ForgeRegistries.BLOCKS.register(block);
 		ForgeRegistries.ITEMS.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
-
+	private static void setRegisterItem(Item block) {
+		
+		ForgeRegistries.ITEMS.register(block);
+	}
 	public static void registerRender() {
 		setRender(iridiumOre);
 		setRender(iridium_Block);
 		setRender(platiumOre);
+		setRender(toriyore);
 		setRender(platium_Block);
+		setRenderItem(toriy);
 	}
-
+	private static void setRenderItem(Item block) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(block, 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+	}
 	private static void setRender(Block block) {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
 	}
---
+++ src/main/java/com/denfop/ssp/common/SSPToriyOre.java
@@ -0,0 +1,50 @@
+package com.denfop.ssp.common;
+
+import com.denfop.ssp.SuperSolarPanels;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Random;
+
+public class SSPToriyOre extends Block {
+	public SSPToriyOre(Material material, String name, float hardness, float resistance, String harvLevel, int level, SoundType soundType) {
+		super(material);
+		setRegistryName(Constants.MOD_ID, name);
+		setUnlocalizedName(name);
+		setHardness(hardness);
+		setResistance(resistance);
+		setHarvestLevel(harvLevel, level);
+		setSoundType(soundType);
+		setCreativeTab(SuperSolarPanels.SSPTab);
+		setLightLevel(0.2F);
+	}
+	@Override
+	@ParametersAreNonnullByDefault
+	 public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	    {
+	        return BlocksRegister.toriy;
+	    }
+	 @Override
+		@ParametersAreNonnullByDefault
+	 public int quantityDroppedWithBonus(int fortune, Random random)
+	    {
+	        return this.quantityDropped(random) + random.nextInt(fortune + 1);
+	    }
+
+	@Override
+	@ParametersAreNonnullByDefault
+	public int getExpDrop(IBlockState state, net.minecraft.world.IBlockAccess world, BlockPos pos, int fortune) {
+		Random rand = world instanceof World ? ((World) world).rand : new Random();
+		if (this.getItemDropped(state, rand, fortune) != Item.getItemFromBlock(this))
+			return MathHelper.getInt(rand, 0, 2);
+		return 0;
+	}
+}
